798886a2bf1000ec1578e2f2a8062da4a321ab7f
ridhiwani <ridhiwanihamza597@gmail.com>
2024-06-14 21:06:03 -0700

    complete header search form functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -8,7 +8,7 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
-router.get('/get', getListings)
+router.get('/get', getListings);
 
 
 export default router;
